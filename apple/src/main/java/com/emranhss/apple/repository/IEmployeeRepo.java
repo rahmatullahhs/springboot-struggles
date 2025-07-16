e82066aeaadab9759c91bb70d77795897e055c5b
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-07-16 21:37:44 +0600

    delete

--- apple/src/main/java/com/emranhss/apple/repository/IEmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.emranhss.apple.repository;
-
-import com.emranhss.apple.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IEmployeeRepo  extends JpaRepository<Employee,Integer> {
-}
