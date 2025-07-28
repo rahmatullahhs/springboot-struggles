56b979ec0a1c801a68a343a5aa5231572ca3acd5
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-07-28 19:06:12 +0600

    aaa

--- crud/src/main/java/com/example/crud/entity/Employee.java
+++ crud/src/main/java/com/example/crud/entity/Employee.java
@@ -1,9 +1,60 @@
 package com.example.crud.entity;
 
-public interface Employee {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private float salary;
+    private String designation;
 
+    public Employee() {
+    }
 
+    public Employee(Long id, String name, float salary, String designation) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.designation = designation;
+    }
 
+    public Employee(String name, float salary, String designation) {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
 }
--- crud/src/main/java/com/example/crud/repository/IEmployeeRepo.java
+++ crud/src/main/java/com/example/crud/repository/IEmployeeRepo.java
@@ -1,4 +1,9 @@
 package com.example.crud.repository;
 
-public class IEmployeeRepo {
+import com.example.crud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEmployeeRepo extends JpaRepository<Employee, Long> {
 }
