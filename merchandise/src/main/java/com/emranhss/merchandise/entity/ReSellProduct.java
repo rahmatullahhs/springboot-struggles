31ebef9b876d563a07cd4e4f4e0bc953d79004b7
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-09-08 19:16:45 +0600

    add

---
+++ merchandise/src/main/java/com/emranhss/merchandise/entity/ReSellProduct.java
@@ -0,0 +1,103 @@
+package com.emranhss.merchandise.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class ReSellProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String details;
+    private String invoice;
+    private Date date;
+    private int qty;
+    private double price;
+    private double paid;
+
+    public ReSellProduct() {
+    }
+
+
+    public ReSellProduct(Long id, String name, String details, String invoice, Date date, int qty, double price, double paid) {
+        this.id = id;
+        this.name = name;
+        this.details = details;
+        this.invoice = invoice;
+        this.date = date;
+        this.qty = qty;
+        this.price = price;
+        this.paid = paid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(String invoice) {
+        this.invoice = invoice;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPaid() {
+        return paid;
+    }
+
+    public void setPaid(double paid) {
+        this.paid = paid;
+    }
+
+
+
+
+}
---
+++ merchandise/src/main/java/com/emranhss/merchandise/service/ReSellProductService.java
@@ -0,0 +1,46 @@
+package com.emranhss.merchandise.service;
+
+import com.emranhss.merchandise.entity.ReSellProduct;
+import com.emranhss.merchandise.repository.ReSellproductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReSellProductService {
+
+    @Autowired
+    private ReSellproductRepo reSellproductRepo;
+
+    public ReSellProduct save(ReSellProduct reSellProduct) {
+        return reSellproductRepo.save(reSellProduct);
+    }
+
+    public List<ReSellProduct> getAllReSellProducts() {
+        return reSellproductRepo.findAll();
+    }
+
+    public Optional<ReSellProduct> getById(Long id) {
+        return reSellproductRepo.findById(id);
+    }
+
+    public ReSellProduct update(Long id, ReSellProduct updatedReSellProduct) {
+        return reSellproductRepo.findById(id).map(existing -> {
+            existing.setName(updatedReSellProduct.getName());
+            existing.setDetails(updatedReSellProduct.getDetails());
+            existing.setInvoice(updatedReSellProduct.getInvoice());
+            existing.setDate(updatedReSellProduct.getDate());
+            existing.setQty(updatedReSellProduct.getQty());
+            existing.setPrice(updatedReSellProduct.getPrice());
+            existing.setPaid(updatedReSellProduct.getPaid());
+            return reSellproductRepo.save(existing);
+        }).orElse(null);
+    }
+
+
+    public void delete(Long id) {
+        reSellproductRepo.deleteById(id);
+    }
+}
